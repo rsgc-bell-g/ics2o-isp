31d39be30aaedd6661405f5b35ed32c1e705a38c
Student <student@gavin-bell.local>
2016-11-22 09:51:51 -0500

    Initial Commit

---
+++ Wishlist test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wishlist test
+//
+//  Created by Gavin Bell on 2016-11-16.
+//  Copyright © 2016 Gavin Bell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

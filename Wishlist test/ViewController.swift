9e1fe21f058641305eb47cebdbd8ed339eecf498
Student <student@gavin-bell.local>
2016-12-10 13:42:24 -0500

    worked on the food review app to practice

--- Wishlist test/ViewController.swift
+++ Wishlist test/ViewController.swift
@@ -1,25 +1,83 @@
-//
 //  ViewController.swift
 //  Wishlist test
-//
 //  Created by Gavin Bell on 2016-11-16.
 //  Copyright © 2016 Gavin Bell. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+// Mark: Properties
+   
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field's user input through delegate callbacks
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
+// Mark: UITextFieldDelegate
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+// Mark: UIImagePickerControllerDelegate
+   
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        // Dismiss the picker if the user canceled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [ String : Any]) {
+        
+        // The info dictionary contains multiple representations of the image, and this ensures the original image is used
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView object to siplay the selected image
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
+    
+// Mark: Actions
+   
 
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from thier photo library
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked not taken
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+
+    @IBAction func setDefaultLabelText(_ sender: Any) {
+        mealNameLabel.text = "Default Text"
+    }
 
 }
 
